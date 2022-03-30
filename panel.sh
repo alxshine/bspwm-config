5dad486561612c12fa4fa606271422af6d3b362e
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-03-30 23:05:43 +0200

    Add panel

--- panel.sh
+++ panel.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
+
+Desktops(){
+  monitors=$(bspc query -D -m "$1" --names)
+  current_monitor=$(bspc query -D -d newest.focused -m "$1" --names)
+  printf "%s" "$monitors" | tr -s '\n' ' ' | sed "s/$current_monitor/ +$current_monitor+ /"
+}
 
 Clock(){
 	TIME=$(date "+%H:%M")
@@ -14,7 +20,13 @@ Language(){
   xkb-switch
 }
 
+Batteries(){
+  printf "BAT: %s %s" $(cat /sys/class/power_supply/BAT0/capacity) `cat /sys/class/power_supply/BAT1/capacity 2>/dev/null`
+}
+
+desktop=$1
+
 while true; do
-	echo -e "%{c}$(Cal)  $(Clock)" "%{r}$(Language)"
-	sleep 0.5s
+	echo -e "%{l}$(Desktops $desktop)" "%{c}$(Cal)  $(Clock)" "%{r}$(Batteries) | $(Language)"
+	sleep 0.1s
 done
